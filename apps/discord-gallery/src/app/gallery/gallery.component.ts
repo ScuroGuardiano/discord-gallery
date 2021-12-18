7e4d573ceeb8f4728e80a560f84625dc933c1e85
ScuroGuardiano <scuroguardiano@gmail.com>
2021-12-18 02:59:42 +0000

    I don't know what I am doing

--- apps/discord-gallery/src/app/gallery/gallery.component.ts
+++ apps/discord-gallery/src/app/gallery/gallery.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { IGuildLinkInfo } from '@discord-gallery/api-interfaces';
+import { LinkInfoService } from './link-info.service';
 
 @Component({
   selector: 'discord-gallery-gallery',
@@ -9,13 +11,21 @@ import { ActivatedRoute } from '@angular/router';
 export class GalleryComponent implements OnInit {
 
   public path = '';
+  public link?: IGuildLinkInfo;
+  public error?: string;
 
   constructor(
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private linkInfo: LinkInfoService
   ) { }
 
   ngOnInit(): void {
     this.path = this.route.snapshot.paramMap.get('linkId') ?? '';
+    this.linkInfo.getLinkInfo(this.path)
+      .subscribe(
+        link => this.link = link,
+        error => this.error = error
+      );
   }
 
 }
--- apps/discord-gallery/src/app/gallery/link-info.service.ts
+++ apps/discord-gallery/src/app/gallery/link-info.service.ts
@@ -1,6 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { IGuildLinkInfo } from '@discord-gallery/api-interfaces';
+import { throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +12,36 @@ export class LinkInfoService {
 
   public getLinkInfo(linkId: string) {
     return this.http.get<IGuildLinkInfo>(`/api/guilds/${linkId}`).pipe(
+      retry(3),
+      catchError(this.handleError)
+    );
+  }
 
-    )
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      console.error(`Can't connect to backend, error:`, error.error);
+      return throwError('Cannot connect to backend. Check your network.');
+    }
+    else {
+      console.error(`Backend returned ${error.status} error code, body: ${error.error}`);
+    }
+
+    if (error.status === 404) {
+      return throwError('Link was not found.');
+    }
+
+    if (error.status === 400 && error.error?.errorType === 'LinkExpiredError') {
+      return throwError('Link expired, create a new one.');
+    }
+
+    if (error.status === 400) {
+      return throwError('Link is invalid.');
+    }
+
+    if (error.status >= 500 && error.status < 600) {
+      return throwError(`Server error: ${error.statusText}.`);
+    }
+
+    return throwError('Uknown error occured.');
   }
 }
